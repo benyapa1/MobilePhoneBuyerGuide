26c98f0256821fa9e2480eb0bd228d7760fe7088
Papada <papada.pdg@gmail.com>
2019-09-30 15:44:19 +0700

    create test double mobile list presenter

--- MobilePhoneBuyerGuideTests/MobileList/MobileListPresenterTests.swift
+++ MobilePhoneBuyerGuideTests/MobileList/MobileListPresenterTests.swift
@@ -24,6 +24,7 @@ class MobileListPresenterTests: XCTestCase {
 
   override func tearDown() {
     super.tearDown()
+    sut = nil
   }
 
   // MARK: - Test setup
@@ -33,14 +34,40 @@ class MobileListPresenterTests: XCTestCase {
   }
 
   // MARK: - Test doubles
-
+    class MobileListViewControllerSpy: MobileListViewControllerInterface {
+        var displayTableViewFromApiCalled = false
+        var displayViewFromSortingDataCalled = false
+        var displayViewByPageCalled = false
+        var displayViewFromDeleteFavCalled = false
+        var displayViewFromChangeFavCalled = false
+        
+        func displayTableViewFromApi(viewModel: MobileList.ShowListMobile.ViewModel) {
+            displayTableViewFromApiCalled = true
+        }
+        
+        func displayViewFromSortingData(viewModel: MobileList.showListWithSorting.ViewModel) {
+            displayViewFromSortingDataCalled = true
+        }
+        
+        func displayViewByPage(viewModel: MobileList.changePage.ViewModel) {
+            displayViewByPageCalled = true
+        }
+        
+        func displayViewFromDeleteFav(viewModel: MobileList.deleteFav.ViewModel) {
+            displayViewFromDeleteFavCalled = true
+        }
+        
+        func displayViewFromChangeFav(viewModel: MobileList.addfav.ViewModel) {
+            displayViewFromChangeFavCalled = true
+        }
+    }
   // MARK: - Tests
 
-  func testSomething() {
+  func testPresentFromAPI() {
     // Given
-
+    
     // When
-
+    
     // Then
   }
 }
